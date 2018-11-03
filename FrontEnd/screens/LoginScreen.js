7c63fee4a7ffea029f15bdd82bf209124c7c540c
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-11-03 01:20:23 -0300

    featch login screen

--- FrontEnd/screens/LoginScreen.js
+++ FrontEnd/screens/LoginScreen.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
-import { View, StyleSheet} from "react-native";
+import { 
+    View, 
+    StyleSheet,
+    Alert,
+    FlatList,
+    ActivityIndicator,
+} from "react-native";
 import { 
     Container, 
     Button, 
     Text, 
-    Label,
-    Input,
-    Item,
+    Spinner,
 } from 'native-base';
+import Field from './components/Field';
 
 class LoginScreen extends Component {
     constructor(props) {
@@ -15,10 +20,74 @@ class LoginScreen extends Component {
         this.state = {
           username: '', password: '',
           username_field_is_bad: false, password_field_is_bad: false,
-          username_field_alerts: [''], password_field_alerts: [''], non_field_alert: ['']
+          username_field_alerts: [''], password_field_alerts: [''], 
+          non_field_alert: [''], loading:false,
         };
     }
-  
+    login = async () => {
+        this.setState({ loading: true })
+        const domain = 'IP';
+        const login_path = `http://${domain}/rest-auth/token-obtain/`;
+        console.log('fetching url:', login_path);
+
+        fetch(login_path, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'username': this.state.username,
+                'password': this.state.password,
+            }),
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            //console.log(JSON.stringify(responseJson));
+            //Campo de username
+            if (responseJson.username != undefined){
+                this.setState({ email_field_alerts: responseJson.username})
+                this.setState({ email_field_is_bad: true })
+            }
+            else{
+                this.setState({ email_field_alerts: ['']})
+                this.setState({ email_field_is_bad: false })
+            }
+
+            //Campo de password
+            if (responseJson.password != undefined){
+                this.setState({ password_field_alerts: responseJson.password})
+                this.setState({ password_field_is_bad: true })
+            }
+            else{
+                this.setState({ password_field_alerts: ['']})
+                this.setState({ password_field_is_bad: false })
+            }
+
+            //Sem campo
+            if (responseJson.non_field_errors != undefined){
+                this.setState({ non_field_alert: responseJson.non_field_errors})
+            }
+            else{
+                this.setState({ non_field_alert: ['']})
+            }
+            //Sucesso
+            if (responseJson.token != undefined||
+                responseJson.key != undefined){
+                    this.props.navigation.navigate('TabHandler', {token:responseJson.token})
+            }
+        })
+        .catch( err => {
+            if (typeof err.text === 'function') {
+                err.text().then(errorMessage => {
+                this.props.dispatch(displayTheError(errorMessage))
+                });
+            } else {
+                Alert.alert("Erro na conexão.");
+                console.log(err)
+            }
+        });
+        //this.setState({ loading: false });
+    }
     render() {
         return (
             <Container style={styles.container}>
@@ -26,26 +95,33 @@ class LoginScreen extends Component {
                     <Text>Login</Text>
                 </View>
                 <View style={styles.fields}> 
-                    <Item stackedLabel last>
-                        <Label>Email/Username</Label>
-                        <Input 
-                            onChangeText={(username) => this.setState({username})}
-                        />
-                    </Item>
-                    <Item stackedLabel last>
-                        <Label>Senha</Label>
-                        <Input 
-                            secureTextEntry
-                            onChangeText={(password) => this.setState({password})}
-                        />
-                    </Item>
+                    <Field
+                        placeholder={"Email/Username"}
+                        badInput={this.state.username_field_is_bad}
+                        fieldAlert={this.state.username_field_alerts}
+                        keyExtractor={'username'}
+                        onChangeText={(username) => this.setState({username})}
+                    />
+                    <Field
+                        placeholder={"Password"}
+                        badInput={this.state.email_field_is_bad}
+                        fieldAlert={this.state.email_field_alerts}
+                        keyExtractor={'password'}
+                        onChangeText={(password) => this.setState({password})}
+                        secureTextEntry
                     
+                    />
+                    <FlatList
+                      data={this.state.non_field_alert}
+                      renderItem={({item}) => <Text style ={{color: 'red'}}>{item}</Text>}
+                      keyExtractor={item => 'non_field_errors'}
+                    />
                 </View>
                 <View style={styles.buttons}>
                     <Button 
                         block 
                         style={styles.button} 
-                        onPress={() => this.props.navigation.navigate('MainScreen')}
+                        onPress={this.login}
                     >
                         <Text> Entrar </Text>
                     </Button>
@@ -64,6 +140,7 @@ class LoginScreen extends Component {
                         <Text> Visitante </Text>
                     </Button>
                 </View>
+                
             </Container>
         );
     }
---
+++ FrontEnd/screens/components/Field.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  AppRegistry,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
+
+export default class Field extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder: '',
+      badInput: false,
+      fieldAlert: [''],
+      keyExtractor: null,
+      onChangeText: '',
+      secureTextEntry:false,
+    };
+  }
+
+  render(){
+
+    if (this.props.badInput){
+      field_style = {
+        //backgroundColor: '#F78181',
+        color: 'red',
+      }
+    }
+    else{ // default button style
+      field_style = {
+        color: 'black',
+        //borderWidth: 2,
+      }
+    }
+  return(
+    <View style={this.props.style}>
+        <Item stackedLabel last>
+            <Label style={field_style}>{this.props.placeholder}</Label>
+            <Input 
+                style={{color: 'black'}}
+                secureTextEntry={this.props.secureTextEntry}
+                onChangeText={this.props.onChangeText}
+            />
+        </Item>
+        <FlatList
+            data={this.props.fieldAlert}
+            renderItem={({item}) => <Text style ={{color: 'red'}}>{item}</Text>}
+            keyExtractor={item => this.props.keyExtractor}
+
+        />
+     </View>
+  )
+  }
+}
