958c3a90c963882d9591bf67cd19fd8db07bd8b3
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-11-11 15:33:20 -0200

    Addition of imagePicker

--- src/screens/registration_screens/CreateProfile.js
+++ src/screens/registration_screens/CreateProfile.js
@@ -16,6 +16,8 @@ import axios from 'axios';
 import { API_URL } from 'react-native-dotenv'
 import { BackHandler } from 'react-native';
 import { Item, Input, Label, Form } from 'native-base';
+import ImagePicker from 'react-native-image-picker';
+
 
 class CreateProfile extends Component {
     constructor(props) {
@@ -23,6 +25,7 @@ class CreateProfile extends Component {
         this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
         this.state = {
           name: '', showAlert:false, showLoading:false,
+          photo: 'https://i.imgur.com/UWQ0GOq.png',
         };
     }
     componentWillMount() {
@@ -38,7 +41,30 @@ class CreateProfile extends Component {
         return true;
     }
     update_photo = () => {
-        Alert.alert('Você apertou a imagem')
+        //Alert.alert('Você apertou a imagem')
+        const options = {
+            title: 'Selecionar foto',
+            storageOptions: {
+              skipBackup: true,
+              path: 'images',
+            },
+        };
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+          
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } 
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else {
+                //const source = { uri: response.uri };
+                // You can also display the image using data:
+                const source = 'data:image/jpeg;base64,' + response.data;
+                this.setState({ photo: source });
+            }
+          });
     }
     create_profile = () => {
         Alert.alert('Você apertou o botão')
@@ -50,7 +76,7 @@ class CreateProfile extends Component {
                 <TouchableHighlight onPress={this.update_photo} style={styles.view_circle}>
                     <View>
                       <Image
-                        source={{ uri: 'https://i.imgur.com/UWQ0GOq.png' }}
+                        source={{ uri: this.state.photo }}
                         style={styles.photo}
                       />
                     </View>
